c92e2b2291e5dce2ebc79ab53107c8d19c3bcb48
simo994q <simonkn1@hotmail.com>
2023-10-10 14:48:05 +0200

    sall

--- nyheder-cms-projekt/src/Components/Categories/Inland.jsx
+++ nyheder-cms-projekt/src/Components/Categories/Inland.jsx
@@ -1,6 +1,18 @@
-
+import { getInland } from "../../queries/getInland"
+import { useQuery } from "@tanstack/react-query"
+import { request } from 'graphql-request'
 
 export const Inland = () => {
+
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['allArticles'],
+        queryFn: async () => request(`https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clnij52rt05rg01ue9cvq8ycn/master`, getInland)
+    })
+
+    if (isLoading) console.log('Loading...');
+
+    if (error) console.log(error.message);
+
     return (
         <></>
     )
